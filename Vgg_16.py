6de82b18c02d3b32cec29a70741a27367a7340a0
Đặng Thái Sơn <sonthaidang1630@gmail.com>
2025-05-08 11:14:46 +0700

    Vgg_16 with fake input

--- Vgg_16.py
+++ Vgg_16.py
@@ -2,54 +2,74 @@ import torch
 import torch.nn as nn
 
 class VGG16(nn.Module):
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=1000):  
         super(VGG16, self).__init__()
+        
         self.features = nn.Sequential(
             # Block 1
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            # assume the input is a rgb image size 224x224 (3,224,244)
+            nn.Conv2d(3, 64, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            
             # Block 2
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            #Block 3
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
+            
+            # Block 3
+            nn.Conv2d(128, 256, kernel_size=3, padding=1), 
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), 
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
             # Block 4
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
-
+            
             # Block 5
-
-
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096), 
+            nn.ReLU(True), 
+            nn.Dropout(),
+            nn.Linear(4096, 4096), nn.ReLU(True), 
+            nn.Dropout(),
+            nn.Linear(4096, num_classes)
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)  # Flatten everything except batch
+        x = self.classifier(x)
+        return x
 
-
-
-# Tạo mô hình
+# initate model
 model = VGG16(num_classes=1000)
 
+input = torch.randn(1, 3, 224, 224)
+
+output = model(input)
+
+# In kích thước output
+print("Output shape:", output.shape)
